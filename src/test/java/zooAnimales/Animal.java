d0d342c10a9e5155e2fcc7ce2a92b83072f13843
DavidToroArboleda <142697635+DavidToroArboleda@users.noreply.github.com>
2023-09-23 19:54:05 -0500

    Revert "Update Animal.java"
    This reverts commit bfb25925110771fd91bc492f4be9d2e9b14dde46.

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -37,14 +37,13 @@ public class Animal {
 	//total por tipo
 	public static String totalPorTipo() {
 	
-		/*
 		int cantmamifero = Mamifero.getListado().size();
 		int cantave = Ave.getListado().size();
 		int cantreptil = Reptil.getListado().size();
 		int cantpez = Pez.getListado().size();
 		int cantanfibio = Anfibio.getListado().size();
 		
-		
+		/*
 		return "Mamiferos: " +cantmamifero+"\n"+
 		"Aves: "+cantave+"\n"+
 		"Reptiles: "+cantreptil+"\n"+
@@ -53,8 +52,8 @@ public class Animal {
 	  */
 		
 		
-		return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\nAves: " + Ave.cantidadAves() + "\nReptiles: " 
-		+ Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() + "\nAnfibios: " + Anfibio.cantidadAnfibios();
+		//return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\nAves: " + Ave.cantidadAves() + "\nReptiles: " 
+		//+ Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() + "Anfibios: " + Anfibio.cantidadAnfibios();
 		
 	}
 	
