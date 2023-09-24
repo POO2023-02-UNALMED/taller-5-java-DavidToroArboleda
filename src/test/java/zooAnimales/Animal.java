31e21309b48929fa8de11ac5a59e4e6bbcfc5bdd
DavidToroArboleda <142697635+DavidToroArboleda@users.noreply.github.com>
2023-09-23 19:54:31 -0500

    Revert "Update Animal.java"
    This reverts commit 6c5e52476ef6e08433300569f9796dd6f5ec0637.

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -38,9 +38,7 @@ public class Animal {
 	public static String totalPorTipo() {
 	
 		return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\nAves: " + Ave.cantidadAves() + "\nReptiles: "
-        + Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() + "Anfibios: " + Anfibio.cantidadAnfibios();
-		
-	}
+        + Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() + "\nAnfibios: " + Anfibio.cantidadAnfibios();}
 	
 	
 	
